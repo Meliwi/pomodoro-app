403458ec59ba53268ce5233a3205a98ab1541b39
Melissa <melissa.gonzalez@correounivalle.edu.co>
2021-10-16 22:14:54 -0500

    handle on change on inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import SetPomodoro from "./components/SetPomodoro";
+
 function App() {
   return (
     <div className="container">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <SetPomodoro></SetPomodoro>
     </div>
   );
 }
--- src/components/SetPomodoro.js
+++ src/components/SetPomodoro.js
@@ -1,24 +1,72 @@
+import { useState } from "react";
+import Button from "./Button";
+
 const SetPomodoro = () => {
+  const [newTimer, setNewTimer] = useState({
+    work: 0.3,
+    shortBreak: 0.2,
+    longBreak: 1,
+    activate: "work",
+  });
 
-    const [newTimer, setNewTimer] = useState({
-        work:0.3,
-        shortBreak: 0.2,
-        longBreak: 1,
-        activate: 'work'
-    })
+  const handleChange = (input) => {
+    const { name, value } = input.target;
+    switch (name) {
+      case "work":
+        setNewTimer({
+          ...newTimer,
+          work: parseInt(value),
+        });
 
-    return(
-        <div className="form-container">
-            {/* No validate means that input shouldn't be validated when submit*/}
-            <form noValidate>
-                <div>
-                    <input className="input" name="work" onChange={handleChange} value={}></input>
-                    <input className="input" name="shortBreak" onChange={handleChange} value={}></input>
-                    <input className="input" name="longBreak" onChange={handleChange} value={}></input>
-                </div>       
-            </form>|
+        break;
+      case "shortBreak":
+        setNewTimer({
+          ...newTimer,
+          shortBreak: parseInt(value),
+        });
+        break;
+      case "longBreak":
+        setNewTimer({
+          ...newTimer,
+          longBreak: parseInt(value),
+        });
+        break;
+      default:
+        break;
+    }
+    console.log(newTimer);
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="form-container">
+      {/* No validate means that input shouldn't be validated when submit*/}
+      <form noValidate>
+        <div>
+          <input
+            className="input"
+            name="work"
+            onChange={handleChange}
+            value={newTimer.work}
+          ></input>
+          <input
+            className="input"
+            name="shortBreak"
+            onChange={handleChange}
+            value={newTimer.shortBreak}
+          ></input>
+          <input
+            className="input"
+            name="longBreak"
+            onChange={handleChange}
+            value={newTimer.longBreak}
+          ></input>
         </div>
-    )
-}
+      </form>
+      <Button callBack={handleSubmit}>title="setTimer"</Button>
+    </div>
+  );
+};
 
 export default SetPomodoro;
